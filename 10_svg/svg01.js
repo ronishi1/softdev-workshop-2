882161c101125a1468ea8d02539fc0d136708d45
Ray <rayonishi11@gmail.com>
2019-03-14 06:44:56 -0400

    10

--- 10_svg/svg01.js
+++ 10_svg/svg01.js
@@ -4,7 +4,6 @@ var clear_but = document.getElementById('but_clear');
 var drawNew = true;
 var clear = () => {
   pic.innerHTML=''
-
 }
 
 clear_but.addEventListener('click',clear);
@@ -14,7 +13,7 @@ var draw = (e) => {
   x = e.offsetX;
   y = e.offsetY;
   var c = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-  c.addEventListener("click",function(e){
+  c.addEventListener("click",function(){
     if(c.getAttribute("fill") == "red") {
       c.setAttribute("fill","green");
     }
@@ -23,10 +22,10 @@ var draw = (e) => {
       c.setAttribute("cx",Math.random()*500);
       c.setAttribute("cy",Math.random()*500);
     }
-    // Prevents drrawing a new circle on top of an existing one
+    // If the user is clicking on the circle, doesn't allow drawing of a circle on top
     drawNew = false;
-  }
-  );
+  });
+  // If the user isn't clicking on a circle, make a new circle where they are clicking
   if(drawNew){
     c.setAttribute("cx",x);
     c.setAttribute("cy", y);
